4610ccce3568b1a16c29599a44da31d685a5f47a
Márcio Sobral <sobralmps@gmail.com>
2026-03-01 06:01:22 -0300

    fix: update default mode to dark in themes configuration

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -5,4 +5,4 @@ export const modes = ['light', 'dark'] as const;
 export type Mode = (typeof modes)[number];
 
 export const defaultTheme: Theme = 'ink';
-export const defaultMode: Mode = 'light';
+export const defaultMode: Mode = 'dark';
